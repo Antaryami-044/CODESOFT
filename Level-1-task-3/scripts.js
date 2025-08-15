a492723eb430596bac97528536be7d919ae004c9
Antaryami-044 <antaryamibabu1234@gmail.com>
2025-08-15 17:18:12 +0530

    updated

---
+++ Level-1-task-3/scripts.js
@@ -0,0 +1,47 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll(".num");
+let displayValue = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        handleInput(this.getAttribute("data-value"));
+    });
+});
+
+// document.addEventListener("keydown", function(event) {
+//     let key = event.key;
+
+//     if (!isNaN(key) || ["+", "-", "*", "/", "."].includes(key)) {
+//         handleInput(key);
+//     }
+//     else if (key === "Enter") {
+//         handleInput("=");
+//     }
+//     else if (key === "Backspace") {
+//         displayValue = displayValue.slice(0, -1);
+//         display.value = displayValue;
+//     }
+//     else if (key === "Escape") {
+//         handleInput("c");
+//     }
+// });
+
+function handleInput(value) {
+    if (value === 'c') {
+        displayValue = "";
+        display.value = "";
+    } 
+    else if (value === "=") {
+        try {
+            display.value = eval(displayValue);
+            displayValue = display.value;
+        } catch {
+            display.value = "Re-entered the number";
+            displayValue = "";
+        }
+    } 
+    else {
+        displayValue += value;
+        display.value = displayValue;
+    }
+}
